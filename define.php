2820b08bea10c26ce2cffa077b1de021c3d3433b
Javad <J.Evazzadeh@live.com>
2016-04-12 08:50:21 +0430

    change to better location

--- define.php
+++ define.php
@@ -91,6 +91,10 @@ else
 	exit("<p>There doesn't seem to be a <code>config.php</code> file. I need this before we can get started.</p>");
 }
 
+if(function_exists('mysqli_fetch_all'))
+{
+  die('you must install mysqlnd enabled!');
+}
 
 // Define Project Methods *****************************************************
 // define object method
