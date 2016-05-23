f36bda4babc8a048c56dcc02273f51cb5fc5895b
Javad <J.Evazzadeh@live.com>
2016-05-23 14:13:20 +0430

    add terms and cats, tags lib to db

--- lib/saloos.php
+++ lib/saloos.php
@@ -6,7 +6,7 @@ namespace lib;
 class saloos
 {
 	// @var saloos core current version
-	const version = '6.6.2';
+	const version = '6.7.0';
 
 	// @var saloos core current commit number
 	// now get it automatically from git commands
