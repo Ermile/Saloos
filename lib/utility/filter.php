cd664dec281e3fa73bba010e127e6db1db9e9c35
baravak <itb.baravak@gmail.com>
2016-10-29 17:23:15 +0330

    meta_decode backslash bugfix

--- lib/utility/filter.php
+++ lib/utility/filter.php
@@ -87,8 +87,11 @@ class filter
 				$to_array = false;
 			}
 			foreach ($json_fields as $key => $value) {
-				$row_value = preg_replace("#[\n]#Ui", "\\n", $_row[$value]);
+				$row_value = preg_replace("#\n#Ui", "\\n", $_row[$value]);
+				$row_value = preg_replace("/\\\([_*])/", "\\\\\\\\$1", $row_value);
+				var_dump($row_value);
 				$json = json_decode($row_value, $to_array);
+				var_dump($json);
 				$_row[$value] = is_null($json) ? $_row[$value] : $json;
 			}
 		}, [$field, $_options]);
