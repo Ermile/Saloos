9e2aef7b081ccd16cbf58b7935d50e9f8a09cbf5
baravak <itb.baravak@gmail.com>
2016-10-29 18:43:46 +0330

    var_dump bugfix :))

--- lib/utility/filter.php
+++ lib/utility/filter.php
@@ -89,9 +89,7 @@ class filter
 			foreach ($json_fields as $key => $value) {
 				$row_value = preg_replace("#\n#Ui", "\\n", $_row[$value]);
 				$row_value = preg_replace("/\\\([_*])/", "\\\\\\\\$1", $row_value);
-				var_dump($row_value);
 				$json = json_decode($row_value, $to_array);
-				var_dump($json);
 				$_row[$value] = is_null($json) ? $_row[$value] : $json;
 			}
 		}, [$field, $_options]);
