9ca6fc0e00e6023c39f282524f4f4dccd21c70ee
Javad <J.Evazzadeh@live.com>
2016-11-11 16:28:18 +0330

    cleaning!

--- lib/router/config.php
+++ lib/router/config.php
@@ -58,21 +58,27 @@ trait config
 		return isset(self::$storage[$name]) ? self::$storage[$name] : null;
 	}
 
-	public static function cache_config(){
+	public static function cache_config()
+	{
 		$class = new \ReflectionClass('\lib\router');
 		$arr = $class->getStaticProperties();
 		self::$cache_array = $arr;
 	}
 
-	public static function get_class_static($naem){
+	public static function get_class_static($naem)
+	{
 		return (isset(self::$cache) && self::$cache) ? self::$cache_array[$naem] : \lib\router::${$naem};
 
 	}
 
-	public static function set_class_static($name, $value){
-		if(isset(self::$cache) &&  self::$cache){
+	public static function set_class_static($name, $value)
+	{
+		if(isset(self::$cache) &&  self::$cache)
+		{
 			self::$cache_array[$name] = $value;
-		}else{
+		}
+		else
+		{
 			\lib\router::${$name} = $value;
 		}
 
@@ -81,11 +87,13 @@ trait config
 	 * PROTOCOL
 	 */
 
-	public static function get_protocol(){
+	public static function get_protocol()
+	{
 		return self::get_class_static('protocol');
 	}
 
-	public static function set_protocol($protocol){
+	public static function set_protocol($protocol)
+	{
 		return self::set_class_static('protocol', $protocol);
 	}
 
@@ -93,7 +101,8 @@ trait config
 	 * URL PROPERTY
 	 */
 
-	public static function add_url_property($value){
+	public static function add_url_property($value)
+	{
 		$array = self::get_class_static('url_array_property');
 		array_unshift($array, $value);
 		self::set_class_static('url_array_property', $array);
@@ -101,8 +110,10 @@ trait config
 	}
 
 
-	public static function check_url_router(){
-		foreach (self::$url_array_property as $key => $value) {
+	public static function check_url_router()
+	{
+		foreach (self::$url_array_property as $key => $value)
+		{
 			$index_property = preg_split("[=]", $value);
 			self::$url_index_property[$index_property[0]] = (isset($index_property[1])) ? $index_property[1] : null;
 		}
@@ -112,7 +123,8 @@ trait config
 	 * MAIN URL
 	 */
 
-	public static function unshift_url($value){
+	public static function unshift_url($value)
+	{
 		$array = self::get_class_static('url_array');
 		array_unshift($array, $value);
 		$string = join($array, '/');
@@ -120,7 +132,8 @@ trait config
 		self::set_class_static('url_array', preg_split("[/]", $string));
 	}
 
-	public static function push_url($value){
+	public static function push_url($value)
+	{
 		$array = self::get_class_static('url_array');
 		array_push($array, $value);
 		$string = join($array, '/');
@@ -128,16 +141,27 @@ trait config
 		self::set_class_static('url_array', preg_split("[/]", $string));
 	}
 
-	public static function remove_url($value){
+
+	/**
+	 * [remove_url description]
+	 * @param  [type] $value [description]
+	 * @return [type]        [description]
+	 */
+	public static function remove_url($value)
+	{
 		$array = self::get_class_static('url_array');
 		$index = array_search($value, $array);
-		if($index !== -1){
+		if($index !== -1)
+		{
 			unset($array[$index]);
 			$string = join($array, '/');
-			if(empty($string)){
+			if(empty($string))
+			{
 				self::set_class_static('url_string', null);
 				self::set_class_static('url_array', array());
-			}else{
+			}
+			else
+			{
 				self::set_class_static('url_string', $string);
 				self::set_class_static('url_array', preg_split("[/]", $string));
 			}
@@ -153,7 +177,8 @@ trait config
 		}
 	}
 
-	public static function shift_url(){
+	public static function shift_url()
+	{
 		$array = self::get_class_static('url_array');
 		array_shift($array);
 		$string = join($array, '/');
@@ -161,7 +186,8 @@ trait config
 		self::set_class_static('url_array', preg_split("[/]", $string));
 	}
 
-	public static function pop_url($value){
+	public static function pop_url($value)
+	{
 		$array = self::get_class_static('url_array');
 		array_pop($array, $value);
 		$string = join($array, '/');
@@ -169,7 +195,8 @@ trait config
 		self::set_class_static('url_array', preg_split("[/]", $string));
 	}
 
-	public static function set_url($value = ''){
+	public static function set_url($value = '')
+	{
 		$string = is_array($value) ? join($array, '/') : $value;
 		$array = is_array($value) ? $value : explode("/", $value);
 		self::set_class_static('url_string', $string);
@@ -180,13 +207,17 @@ trait config
 	 * GETTER
 	 */
 
-	public static function get_property($search = null, $index, $property = false, $splitor = '/'){
+	public static function get_property($search = null, $index, $property = false, $splitor = '/')
+	{
 		// if user pass nothing or null then splite address array with slash /
 		if($search === null)
+		{
 			return join($index, $splitor);
-
+		}
 		elseif($search === -1)
+		{
 			return $index;
+		}
 
 		// if user pass undeline '_' or space ' ' as search use it as splitor
 		elseif($search === '_' || $search === ' ')
@@ -196,7 +227,9 @@ trait config
 			$tmp_strpos = strpos($tmp_result,'=');
 			// then splite from start until assign character
 			if($tmp_strpos!==false)
+			{
 				$tmp_result = substr($tmp_result, 0, $tmp_strpos);
+			}
 
 			if(!$tmp_result && $search === '_')
 			{
@@ -210,38 +243,48 @@ trait config
 				}
 			}
 
-			// var_dump($tmp_result);
 			return $tmp_result;
 		}
 
 		elseif(preg_match("/^\d+$/", $search))
 		{
 			if(isset($index[$search]))
+			{
 				return $index[$search];
+			}
 			else
+			{
 				return null;
+			}
 		}
 		elseif($property !== false)
 		{
 			if(isset($property[$search]))
+			{
 				return $property[$search];
+			}
 			else
+			{
 				return null;
+			}
 		}
 
 		return null;
 	}
 
-	public static function get_url_property($search = null){
+	public static function get_url_property($search = null)
+	{
 		return self::get_property($search, self::get_class_static('url_array_property'), self::get_class_static('url_index_property'));
 	}
 
-	public static function get_url($search = null){
+	public static function get_url($search = null)
+	{
 		return self::get_property($search, self::get_class_static('url_array'));
 
 	}
 
-	public static function get_real_url($search = null){
+	public static function get_real_url($search = null)
+	{
 		return self::get_property($search, self::get_class_static('real_url_array'));
 	}
 
@@ -294,13 +337,17 @@ trait config
 		return $result;
 	}
 
-	public static function get_domain($search = null){
+	public static function get_domain($search = null)
+	{
 		return self::get_property($search, self::get_class_static('domain'), false, '.');
 	}
 
-	public static function set_domain($domain = null){
+	public static function set_domain($domain = null)
+	{
 		if(!$domain)
+		{
 			$domain = self::get_root_domain();
+		}
 		self::set_class_static('domain', is_array($domain) ? $domain : explode('.', $domain));
 	}
 
@@ -311,7 +358,9 @@ trait config
 	public static function set_sub_domain($_subdomain)
 	{
 		if(is_array($_subdomain))
+		{
 			$_subdomain = implode('.', $_subdomain);
+		}
 
 		$mydomain = self::get_domain(-1);
 		array_unshift($mydomain, $_subdomain);
@@ -319,43 +368,52 @@ trait config
 		self::set_domain($mydomain);
 
 		if(!is_array($_subdomain))
+		{
 			$_subdomain = explode('.', $_subdomain);
+		}
 
 		self::set_class_static('sub_domain', $_subdomain);
 	}
 
-	public static function get_sub_domain($search = null){
+	public static function get_sub_domain($search = null)
+	{
 		return self::get_property($search, self::get_class_static('sub_domain'), false, '.');
 	}
 
 
-
-	public static function get_class(){
+	public static function get_class()
+	{
 		return self::get_class_static('class');
 	}
 
-	public static function get_method(){
+	public static function get_method()
+	{
 		return self::get_class_static('method');
 	}
 
-	public static function set_class($class){
-		// if(self::$method !== 'home'){
+	public static function set_class($class)
+	{
+		// if(self::$method !== 'home')
+		// {
 		// 	self::add_url_property(self::$method);
 		// }
-		// if(self::$class !== 'home'){
+		// if(self::$class !== 'home')
+		// {
 		// 	self::add_url_property(self::$class);
 		// }
 		return self::set_class_static('class', $class);
 	}
 
-	public static function set_method($method){
+	public static function set_method($method)
+	{
 		// if(self::$method !== 'home'){
 		// 	self::add_url_property(self::$method);
 		// }
 		return self::set_class_static('method', $method);
 	}
 
-	public static function set_repository($repository, $use_root_dir = true){
+	public static function set_repository($repository, $use_root_dir = true)
+	{
 		$repository = ($use_root_dir)? root.$repository : $repository;
 
 		$repository = (preg_match("/\/$/", $repository)) ? $repository : $repository.'/';
@@ -364,7 +422,8 @@ trait config
 		self::set_repository_name(end($addr));
 	}
 
-	public static function set_repository_hasan($repository, $dont_use_root_dir = false){
+	public static function set_repository_hasan($repository, $dont_use_root_dir = false)
+	{
 		$repository = ($dont_use_root_dir)? $dont_use_root_dir.$repository : root.$repository;
 
 		$repository = (preg_match("/\/$/", $repository)) ? $repository : $repository.'/';
@@ -373,26 +432,31 @@ trait config
 		self::set_repository_name(end($addr));
 	}
 
-	public static function get_repository(){
+	public static function get_repository()
+	{
 		return self::$repository;
 	}
 
-	public static function set_repository_name($name){
+	public static function set_repository_name($name)
+	{
 		// if(!defined($name)){
 		// 	define($name, '/'.self::get_repository().'/');
 		// }
 		self::$repository_name = $name;
 	}
 
-	public static function get_repository_name(){
+	public static function get_repository_name()
+	{
 		return self::$repository_name;
 	}
 
-	public static function set_controller($controller){
+	public static function set_controller($controller)
+	{
 		self::$controller = $controller;
 	}
 
-	public static function get_controller(){
+	public static function get_controller()
+	{
 		return self::$controller;
 	}
 
@@ -424,15 +488,21 @@ trait config
 			case 'host':
 				$myurl   = parse_url($_url, PHP_URL_HOST);
 				if($_type === 'host')
+				{
 					return $myurl;
+				}
 
 				if (isset($myurl) && $myurl)
 				{
 					$myurl   = explode('.', $myurl);
 					if(count($myurl)>=$myplace)
+					{
 						$myurl   = $myurl[count($myurl)-$myplace];
+					}
 					else
+					{
 						$myurl = false;
+					}
 				}
 				else
 					$myurl = null;
@@ -441,7 +511,9 @@ trait config
 			// retun the path of url
 			case 'path':
 				if($_url === 'current')
+				{
 					$_url = isset($_SERVER['REQUEST_URI'])?  $_SERVER['REQUEST_URI']:  null;
+				}
 
 				// clear url
 				$_url  = self::urlfilterer($_url);
@@ -502,25 +574,33 @@ trait config
 			$_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
 		}
 		elseif (isset($_SERVER['HTTP_CLIENT_IP']))
+		{
 			$ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-
+		}
 		elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+		{
 			$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-
+		}
 		elseif(isset($_SERVER['HTTP_X_FORWARDED']))
+		{
 			$ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-
+		}
 		elseif(isset($_SERVER['HTTP_FORWARDED_FOR']))
+		{
 			$ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-
+		}
 		elseif(isset($_SERVER['HTTP_FORWARDED']))
+		{
 			$ipaddress = $_SERVER['HTTP_FORWARDED'];
-
+		}
 		elseif(isset($_SERVER['REMOTE_ADDR']))
+		{
 			$ipaddress = $_SERVER['REMOTE_ADDR'];
-
+		}
 		else
+		{
 			$ipaddress = null;
+		}
 
 		if($_change)
 		{
@@ -528,8 +608,6 @@ trait config
 			$ipaddress = sprintf("%u",ip2long( $ipaddress ));
 			// $ipaddress = ip2long( $ipaddress );
 		}
-
-
 
 		return $ipaddress;
 	}
