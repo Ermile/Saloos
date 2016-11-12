3090037b6660051d21306858cbc9a189ded6b9fd
Reza Mohiti <rm.biqarar@gmail.com>
2016-11-12 19:48:43 +0330

    add tld library

---
+++ lib/utility/location/tld.php
@@ -0,0 +1,69 @@
+<?php
+namespace lib\utility\location;
+
+class tld
+{
+
+	public static $data =
+	[
+		'ir'  => ['name' => 'ir', 'lang' => 'fa'],
+		'com' => ['name' => 'com', 'lang' => 'en']
+	];
+
+
+	/**
+	 * get lost of tld
+	 */
+	public static function list($_request = null, $_index = null)
+	{
+		if($_request === null)
+		{
+			return self::$data;
+		}
+		else
+		{
+			if($_index === null)
+			{
+				return array_column(self::$data, $_request);
+			}
+			else
+			{
+				return array_column(self::$data, $_index, $_request);
+			}
+		}
+	}
+
+
+	/**
+	 * check duage exist and return true or false
+	 * @param  [type] $_tld   [description]
+	 * @param  string $_column [description]
+	 * @return [type]          [description]
+	 */
+	public static function check($_tld, $_column = 'name')
+	{
+		$tld_list = array_column(self::$data, $_column);
+		if(in_array($_tld, $tld_list))
+		{
+			return true;
+		}
+		return false;
+	}
+
+
+	/**
+	 * get tld
+	 *
+	 * @param      <type>  $_key      The key
+	 * @param      string  $_request  The request
+	 */
+	public static function get($_key, $_request = 'lang')
+	{
+		if(isset(self::$data[$_key]))
+		{
+			return self::$data[$_key][$_request];
+		}
+		return null;
+	}
+}
+?>
