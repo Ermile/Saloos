60b6e70003b5cc700a768a51e28956fe2df56f84
Reza Mohiti <rm.biqarar@gmail.com>
2016-11-12 14:55:07 +0330

    add twig number_format function

--- lib/view.php
+++ lib/view.php
@@ -111,6 +111,7 @@ class view
 		$this->add_twig_function('tags');
 		$this->add_twig_function('comments');
 		$this->add_twig_function('similar_post');
+		$this->add_twig_function('number_format');
 
 
 
