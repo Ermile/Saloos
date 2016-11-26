569f7f9f4f77c51f7c2e5a7df4c1f12944cf2eda
baravak <itb.baravak@gmail.com>
2016-11-26 09:01:50 +0330

    begin query return as objects

---
+++ lib/db/return.php
@@ -0,0 +1,55 @@
+<?
+namespace lib\db;
+
+class return
+{
+	public $return;
+
+	public function is_ok()
+	{
+		return get_ok() === true;
+	}
+
+	public function __call($_name, $_args)
+	{
+		if(preg_match("/^set_(.*)$/", $_name, $peroperty))
+		{
+			if(count($_args) > 1)
+			{
+				if(!isset($this->return[$peroperty[1]]))
+				{
+					$this->return[$peroperty[1]] = array();
+				}
+				elseif(!is_array($this->return[$peroperty[1]]))
+				{
+					$this->return[$peroperty[1]] = [$this->return[$peroperty[1]]];
+				}
+				$this->return[$peroperty[1]][$_args[0]] = $_args[1];
+			}
+			else
+			{
+				$this->return[$peroperty[1]] = $_args[0];
+			}
+		}
+		elseif(preg_match("/^get_(.*)$/", $_name, $peroperty))
+		{
+			if(!isset($this->return[$peroperty[1]]))
+			{
+				return null;
+			}
+			if(count($_args) > 0)
+			{
+				if(!is_array($this->return[$peroperty[1]]))
+				{
+					return null;
+				}
+				return $this->return[$peroperty[1]][$_args[0]];
+			}
+			else
+			{
+				return $this->return[$peroperty[1]];
+			}
+		}
+	}
+}
+?>
