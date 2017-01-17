0a949854125628dfa291416c5f21d8789167b743
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-01-17 14:29:45 +0330

    change location of sql log

--- lib/db/log.php
+++ lib/db/log.php
@@ -8,15 +8,15 @@ trait log
 	 * @param  [type] $_text [description]
 	 * @return [type]        [description]
 	 */
-	public static function log($_text, $_time = null)
+	public static function log($_text, $_time = null, $_name = 'db.sql')
 	{
 		$classes  = (array_column(debug_backtrace(), 'file'));
 		if(DEBUG)
 		{
-			$fileAddr = root.'public_html/files/';
+			$fileAddr = database.'log/';
 			\lib\utility\file::makeDir($fileAddr, null, true);
 			// set file address
-			$fileAddr .= 'db.log';
+			$fileAddr .= $_name;
 			$my_text  = "\n#". str_repeat("-", 70). ' '. urldecode($_SERVER['REQUEST_URI']);
 			$my_text .= "\n#". $_time. "s";
 			$my_text .= "\n#". round($_time*1000) . "ms";
