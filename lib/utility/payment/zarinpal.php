ec4c9dfe3a5dfc77769857714116a88eead61228
Reza <rm.biqarar@gmail.com>
2017-02-21 12:19:47 +0330

    verify zarinpal transactions

--- lib/utility/payment/zarinpal.php
+++ lib/utility/payment/zarinpal.php
@@ -77,76 +77,7 @@ class zarinpal
 
             $result = $client->PaymentRequest($request);
 
-            switch ($result->Status)
-            {
-                case -1:
-                    $msg = T_("The submited inforamation are incomplete.");
-                    break;
-
-                case -2:
-                    $msg = T_("IP or merchant code of the host is incorrect");
-                    break;
-
-                case -3:
-                    $msg = T_("Due to Shaparak's limitations, It's impossible to pay the specified amount");
-                    break;
-
-                case -4:
-                    $msg = T_("The host level is below silver level");
-                    break;
-
-                case -11:
-                    $msg = T_("Nothing found for the specified request");
-                    break;
-
-                case -12:
-                    $msg = T_("It's impossible to edit the request");
-                    break;
-
-                case -21:
-                    $msg = T_("No financial operation found for this transaction");
-                    break;
-
-                case -22:
-                    $msg = T_("Transaction faild");
-                    break;
-
-                case -33:
-                    $msg = T_("The specified transaction amount does not match with the payed amount");
-                    break;
-
-                case -34:
-                    $msg = T_("Highest amount of transaction is passed as a result of number or amount");
-                    break;
-
-                case -40:
-                    $msg = T_("Access unavilable to method");
-                    break;
-
-                case -41:
-                    $msg = T_("Invalid information is sent for AdditionalData");
-                    break;
-
-                case -42:
-                    $msg = T_("Valid lifespan of ID must be between 30 minutes to 45 days");
-                    break;
-
-                case -54:
-                    $msg = T_("The request is archived");
-                    break;
-
-                case 100:
-                    $msg = T_("Operation was successfully done");
-                    break;
-
-                case 101:
-                    $msg = T_("Payment operation was successfull and PatmentVerification of the transaction has already done");
-                    break;
-
-                default:
-                    $msg = T("The error code is :code", ['code' => $result->Status]);
-                    break;
-            }
+            $msg = self::msg($result->Status);
 
             if ($result->Status == 100)
             {
@@ -173,6 +104,148 @@ class zarinpal
     public static function verify($_args = [])
     {
 
+        $default_args =
+        [
+            'MerchantID' => null,
+            'Authority'  => null,
+            'Amount'     => null,
+            'Status'     => null,
+        ];
+
+        $_args = array_merge($default_args, $_args);
+
+
+        if(!$_args['MerchantID'])
+        {
+            return debug::error(T_("The MerchantID is required"), 'MerchantID', 'arguments');
+        }
+
+        if(!$_args['Amount'])
+        {
+            return debug::error(T_("The Amount is required"), 'Amount', 'arguments');
+        }
+
+        if(!$_args['Authority'])
+        {
+            return debug::error(T_("The Authority is required"), 'Authority', 'arguments');
+        }
+
+        // if($_args['Status'] == 'NOK')
+        // {
+        //     return debug::error(T_("The user cancel the transaction or transaction is faild"), 'Status', 'arguments');
+        // }
+
+
+        $request               = [];
+        $request['MerchantID'] = $_args['MerchantID'];
+        $request['Amount']     = $_args['Amount'];
+        $request['Authority']  = $_args['Authority'];
+
+        try
+        {
+            $client = @new \soapclient('https://de.zarinpal.com/pg/services/WebGate/wsdl');
+
+            $result = $client->PaymentVerification($request);
+            $msg = self::msg($result->Status);
+
+            if ($result->Status == 100)
+            {
+                return true;
+            }
+            else
+            {
+                debug::error($msg);
+                return false;
+            }
+        }
+        catch (SoapFault $e)
+        {
+            return debug::error(T_("Error in load web services"));
+        }
+    }
+
+
+    /**
+     * payment msg
+     *
+     * @param      <type>  $_status  The status
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
+    private static function msg($_status)
+    {
+        $msg = null;
+        switch ($_status)
+        {
+            case -1:
+                $msg = T_("The submited inforamation are incomplete.");
+                break;
+
+            case -2:
+                $msg = T_("IP or merchant code of the host is incorrect");
+                break;
+
+            case -3:
+                $msg = T_("Due to Shaparak's limitations, It's impossible to pay the specified amount");
+                break;
+
+            case -4:
+                $msg = T_("The host level is below silver level");
+                break;
+
+            case -11:
+                $msg = T_("Nothing found for the specified request");
+                break;
+
+            case -12:
+                $msg = T_("It's impossible to edit the request");
+                break;
+
+            case -21:
+                $msg = T_("No financial operation found for this transaction");
+                break;
+
+            case -22:
+                $msg = T_("Transaction faild");
+                break;
+
+            case -33:
+                $msg = T_("The specified transaction amount does not match with the payed amount");
+                break;
+
+            case -34:
+                $msg = T_("Highest amount of transaction is passed as a result of number or amount");
+                break;
+
+            case -40:
+                $msg = T_("Access unavilable to method");
+                break;
+
+            case -41:
+                $msg = T_("Invalid information is sent for AdditionalData");
+                break;
+
+            case -42:
+                $msg = T_("Valid lifespan of ID must be between 30 minutes to 45 days");
+                break;
+
+            case -54:
+                $msg = T_("The request is archived");
+                break;
+
+            case 100:
+                $msg = T_("Operation was successfully done");
+                break;
+
+            case 101:
+                $msg = T_("Payment operation was successfull and PatmentVerification of the transaction has already done");
+                break;
+
+            default:
+                $msg = T("The error code is :code", ['code' => $result->Status]);
+                break;
+        }
+        return $msg;
     }
 
 }
